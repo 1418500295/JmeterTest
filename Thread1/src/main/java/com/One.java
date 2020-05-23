2129f50cc96929bbf7f2b910d56f03fed08b394f
daine <daine0513@gmail.com>
2020-05-23 09:46:32 +0800

    daine update

---
+++ Thread1/src/main/java/com/One.java
@@ -0,0 +1,29 @@
+package com;
+
+import java.io.File;
+
+public class One {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\ASUS\\JmeterTest\\Thread1\\src\\main\\java\\com\\a.txt";
+        File file  = new File(path);
+        //判断文件是否存在
+        System.out.println(file.exists());
+        //获取文件绝对路径
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getPath());
+        //获取文件名称
+        System.out.println(file.getName());
+        //获取文件大小
+        System.out.println(file.length());
+
+        System.out.println(file.getAbsoluteFile());
+        //判断是否是个目录
+        System.out.println(file.isDirectory());
+        //获取其父目录的路径
+        System.out.println(file.getParent());
+
+
+
+    }
+
+}
