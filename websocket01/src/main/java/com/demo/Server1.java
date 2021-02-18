b7849e62d2aabecddbe31b940a1e5ca9e311bce5
1418500295 <45054883+1418500295@users.noreply.github.com>
2021-02-18 14:32:55 +0400

    Update Server1.java

--- websocket01/src/main/java/com/demo/Server1.java
+++ websocket01/src/main/java/com/demo/Server1.java
@@ -1,4 +1,30 @@
 package com.demo;
 
 public class Server1 {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1243);
+        Socket socket = serverSocket.accept();
+        System.out.println("新的连接建立");
+        BufferedReader bufferedReader;
+        InputStreamReader inputStreamReader;
+        BufferedWriter bufferedWriter;
+        OutputStreamWriter outputStreamWriter;
+        while (true){
+            InputStream in;
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            System.out.println(socket.getInetAddress()+":"+bufferedReader.readLine());
+            OutputStream out;
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+            System.out.println("回复");
+            Scanner scanner = new Scanner(System.in);
+            bufferedWriter.write(scanner.nextLine()+"\n");
+            bufferedWriter.flush();
+
+        }
+
+
+    }
 }
