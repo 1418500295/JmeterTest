c64b88ae0c055cf4ae67d40cd6201352b5dbb2b4
1418500295 <45054883+1418500295@users.noreply.github.com>
2021-02-18 14:35:00 +0400

    Update One.java

--- websocket01/src/main/java/com/demo/One.java
+++ websocket01/src/main/java/com/demo/One.java
@@ -1,4 +1,25 @@
 package com.demo;
 
 public class One {
+  public static void main(String[] args) throws IOException {
+        Socket socket  = new Socket("127.0.0.1",1243);
+        System.out.println("连接成功");
+        BufferedWriter bufferedWriter;
+        OutputStreamWriter outputStreamWriter;
+        BufferedReader bufferedReader;
+        InputStreamReader inputStreamReader;
+        while (true){
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("回复");
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+            bufferedWriter.write(scanner.nextLine()+"\n");
+            bufferedWriter.flush();
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+            System.out.println(socket.getInetAddress()+":"+bufferedReader.readLine());
+
+        }
+
+    }
 }
