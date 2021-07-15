fb99c8b750327cb27d4a1ce7a12030564c1038f6
1418500295 <45054883+1418500295@users.noreply.github.com>
2021-07-15 12:42:13 +0400

    Create CurrentTimeMillisClock.java

---
+++ Thread1/src/main/java/com/CurrentTimeMillisClock.java
@@ -0,0 +1,26 @@
+private static final CurrentTimeMillisClock MILLIS_CLOCK = new CurrentTimeMillisClock(1);
+    private final long precision;
+    private final AtomicLong now;
+
+    private CurrentTimeMillisClock(long precision) {
+        this.precision = precision;
+        now = new AtomicLong(System.currentTimeMillis());
+        scheduleClockUpdating();
+    }
+
+    public static CurrentTimeMillisClock millisClock() {
+        return MILLIS_CLOCK;
+    }
+
+    private void scheduleClockUpdating() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(runnable -> {
+            Thread thread = new Thread(runnable, "system.clock");
+            thread.setDaemon(true);
+            return thread;
+        });
+        scheduler.scheduleAtFixedRate(() -> now.set(System.currentTimeMillis()), precision, precision, TimeUnit.MILLISECONDS);
+    }
+
+    public long now() {
+        return now.get();
+    }
