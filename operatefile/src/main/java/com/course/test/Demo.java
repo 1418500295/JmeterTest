8188c4e295229087f44e0901fc6d40bbf02fe4ca
1418500295 <45054883+1418500295@users.noreply.github.com>
2023-04-06 08:20:32 +0800

    Update Demo.java

--- operatefile/src/main/java/com/course/test/Demo.java
+++ operatefile/src/main/java/com/course/test/Demo.java
@@ -36,6 +36,26 @@ public class Demo {
         while ((line = bufferedReader.readLine() ) != null){
             System.out.println(line);
         }
+        
+        
+        File file = new File("C:\\Users\\14185\\IdeaProjects\\myautitest\\a.txt");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+        String line = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = bufferedReader.readLine() ) != null){
+            stringBuilder.append(line);
+            String[] s = line.split(",");
+            System.out.println(s[0]);
+
+        }
+//        for (int i = 0; i < 5; i++) {
+//            String s;
+//            String c = String.format("%s%s",i,"1");
+//            s = "name="+i+",age="+c+"\n";
+//            FileOutputStream fileOutputStream = new FileOutputStream(file,true);
+//            fileOutputStream.write(s.getBytes());
+//            fileOutputStream.close();
 
 
 
