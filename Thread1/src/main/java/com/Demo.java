8035cf3f5495347d1e1ab69c45e903cfa98dde4c
1418500295 <45054883+1418500295@users.noreply.github.com>
2023-04-09 15:23:40 +0800

    Update Demo.java

--- Thread1/src/main/java/com/Demo.java
+++ Thread1/src/main/java/com/Demo.java
@@ -49,6 +49,44 @@ public class Demo {
             System.out.println("这是普通代码块");
         }
     }
+    
+    
+    public static boolean isNumber(String content){
+        String reg = "^-?[0-9]+(.[0-9]+)?$";
+        return content.matches(reg);
+        
+    }
+
+    public static boolean isInteger(String content){
+        return content.matches("[0-9]+$");
+    }
+    
+    System.out.println("请输入要执行的接口名：");
+        methodName = scanner.next();
+        while (true) {
+            if (!(list.contains(methodName))) {
+                System.out.println("请输入正确的接口");
+                methodName = scanner.next();
+                if (list.contains(methodName)) {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        System.out.println("请输入并发数量(只可以为正整数)：");
+        String input = scanner.next();
+        while (true){
+            if (!(isNumber(input) && Integer.parseInt(input)>0) && isInteger(input)){
+                System.out.println("并发数必须是正整数");
+                input = scanner.next();
+                if ((isNumber(input) && Integer.parseInt(input)>0 && isInteger(input))){
+                    break;
+                }
+            }else {
+                break;
+            }
+        }
 
 
 
