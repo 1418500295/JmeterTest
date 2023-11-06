88d73fd21bf520a0ad25e1a21ea2478d1f8ae46d
1418500295 <45054883+1418500295@users.noreply.github.com>
2023-11-06 17:50:19 +0800

    Update Two.java

--- Thread1/src/main/java/com/Two.java
+++ Thread1/src/main/java/com/Two.java
@@ -13,3 +13,53 @@ public class Two {
 
 
 }
+
+1.jmeter:将异常响应信息写入到jmeter.log,方便排查错误：
+
+package org.example;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+try{
+	String statusCode = prev.getResponseCode();
+	if(!statusCode.equals("200")){
+		log.error("response code is :"+statusCode);
+	}else{
+		String res = prev.getResponseDataAsString();
+		JSONObject js = OrderRandom.strToJson(res);
+		String code = js.getString("code");
+		if(!code.equals("200")){
+			log.error("response is :"+res);
+		}
+	}
+}catch(Exception e){
+	throw new RuntimeException(e);
+}
+
+
+2.jmeter将响应的数据追加写入csv文件：
+package org.example;
+
+import com.alibaba.fastjson.*;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Random;
+import org.apache.commons.lang.StringUtils;
+
+try{
+	String a = vars.get("data");
+	String data = StringUtils.strip(a,"[]");
+
+	log.info("---data---"+data);
+	FileWriter fileWriter = new FileWriter(new File("test.csv"), true);
+	BufferedWriter writer = new BufferedWriter(fileWriter);
+	if(data != "null" && data != "" && data != null){
+		writer.write(data+"\n");
+		writer.flush();
+		writer.close();
+	}
+}catch(Exception e){
+	throw new Exception(e);
+}
+
+
